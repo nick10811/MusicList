8e96a32e44f66d329e7758766fb5fee60fcc69b6
Nick Yang <nick10811@gmail.com>
2022-03-30 21:00:40 +0800

    Project init

---
+++ MusicSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearch
+//
+//  Created by Nick Yang on 2022/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
